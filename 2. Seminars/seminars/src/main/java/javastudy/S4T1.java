3dca8bc7c050ab29ae8fe5e95693d9dd08d61e10
WindDancer <v.b.winddancer@gmail.com>
2022-09-26 19:55:42 +0200

    s4 is done

---
+++ 2. Seminars/seminars/src/main/java/javastudy/S4T1.java
@@ -0,0 +1,22 @@
+package javastudy;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class S4T1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        LinkedList<Integer> list1 = new LinkedList<>();
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            list.add(list.size()/2, i);
+        }
+        System.out.println(System.currentTimeMillis() - t1);
+
+        long t2 = System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            list1.add(list1.size()/2, i);
+        }
+        System.out.println(System.currentTimeMillis() - t2);
+    }
+}
---
+++ 2. Seminars/seminars/src/main/java/javastudy/S4T4.java
@@ -0,0 +1,34 @@
+package javastudy;
+
+import java.util.LinkedList;
+
+/*
+1) Написать метод, который принимает массив элементов, помещает их в стэк и выводит на консоль содержимое стэка.
+2) Написать метод, который принимает массив элементов, помещает их в очередь и выводит на консоль содержимое очереди.
+ */
+public class S4T4 {
+    public static void main(String[] args) {
+        Object[] obj = {1,2,3,4,5,6,7,8,9,10};
+        AddStack(obj);
+        System.out.println("----");
+        AddQueue(obj);
+    }
+    public static void AddStack(Object[] obj) {
+        LinkedList<Object> lobj = new LinkedList<>();
+        for (Object o: obj) {
+            lobj.push(o);
+        }
+        System.out.println(lobj);
+        lobj.pop();
+        System.out.println(lobj);
+    }
+    public static void AddQueue(Object[] obj) {
+        LinkedList<Object> lqueue = new LinkedList<>();
+        for (Object o: obj) {
+            lqueue.offer(o);
+        }
+        System.out.println(lqueue);
+        lqueue.poll();
+        System.out.println(lqueue);
+    }
+}
